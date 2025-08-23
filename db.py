1c8c6206e905e07080eb18ab032e07f387085411
Saielpatil359 <saielpatil88@gmail.com>
2025-08-24 00:47:32 +0530

    Update db.py

--- db.py
+++ db.py
@@ -7,5 +7,6 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 
 def init_db():
-    from .models import Text, Result  # ensure models are imported for table creation
+    from models import Text, Result  # ensure models are imported for table creation
     SQLModel.metadata.create_all(engine)
+
