49e0eef76953bb535d2513e97f8f18ed86b52125
Saielpatil359 <saielpatil88@gmail.com>
2025-09-08 20:23:20 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./typing.db"
+DATABASE_URL = "postgresql://typingpara_user:WIiK9P3q7dqCh1mr0uF3pNISugKZMLjc@dpg-d2veq0buibrs738hjarg-a/typingpara"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
@@ -9,4 +9,5 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 def init_db():
     from models import Text, Result  # ensure models are imported for table creation
     SQLModel.metadata.create_all(engine)
+
 
